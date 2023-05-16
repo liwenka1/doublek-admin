8622938ef6ede346bfc58b572f64a91d1ede4e19
liwenkai <2020583117@qq.com>
2023-05-16 22:13:40 +0800

    feat: 初始化

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue' //引入图标
+import 'element-plus/dist/index.css' //引入样式
+import 'virtual:svg-icons-register'
+import 'uno.css'
+import { createPinia } from 'pinia'
+import router from '@/router'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { UserConfig, ConfigEnv, loadEnv, defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path' //这个path用到了上面安装的@types/node
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import UnoCSS from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]'
+      }),
+      UnoCSS({
+        /* options */
+      })
+    ],
+    //这里进行配置别名
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // @代替src
+        '#': path.resolve('./types') // #代替types
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/styles/index.scss";'
+        }
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_APP_PORT),
+      open: true, // 运行是否自动打开浏览器
+      proxy: {
+        // 反向代理解决跨域
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://vapi.youlai.tech', // 线上接口地址
+          // target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '') // 替换 /dev-api 为 target 接口地址
+        }
+      }
+    }
+  }
+})
